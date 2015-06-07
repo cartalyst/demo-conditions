aee679aa4e37399ec5488b08d3e377fe4176e620
Amanda Romero <amanda@builtnoble.com>
2015-06-07 15:11:29 -0600

    view format cleanup
    Signed-off-by: Amanda Romero <amanda@builtnoble.com>

--- resources/views/conditions/inclusive.blade.php
+++ resources/views/conditions/inclusive.blade.php
@@ -1,67 +1,5 @@
 @extends('layouts.default')
-
-@section('scripts')
-<script>
-var self = {};
-
-self.myCodeMirror = CodeMirror.fromTextArea(document.getElementById('textarea'), {
-	lineNumbers: true,
-	matchBrackets: true
-});
-
-self.myCodeMirror1 = CodeMirror.fromTextArea(document.getElementById('textarea1'), {
-	lineNumbers: true,
-	matchBrackets: true
-});
-
-$('button').on('click', function(e)
-{
-	e.preventDefault();
-
-	mirror = $(this).data('mirror');
-
-	var data = $.parseJSON(self[mirror].getValue());
-
-	console.log($.isArray(data));
-
-	if ($.isArray(data))
-	{
-		var oldData = data;
-		data = {};
-
-		$.each(oldData, function(i)
-		{
-			data[i] = oldData[i];
-		});
-	}
-
-	$.ajax({
-		url: 'process',
-		data: data,
-		type: 'post',
-	}).done(function(res) {
-
-		$('h1 .tax').text(res.tax);
-		$('h1 .price').text(res.price);
-
-		$('.price').counterUp({
-			delay: 5,
-			time: 500
-		});
-
-		$('.tax').counterUp({
-			delay: 5,
-			time: 500
-		});
-	});
-
-});
-
-</script>
-@stop
-
 @section('content')
-
 	<div class="jumbotron text-center">
 		<h1><small>Inc. Tax</small> $<span class="tax">0</span></h1>
 		<h1><small>Total</small> $<span class="price">0</span></h1>
@@ -71,7 +9,6 @@ $('button').on('click', function(e)
 
 	<div class="row">
 		<div class="col-md-6">
-
 <textarea id="textarea" cols="20" rows="10" class="form-control default">
 [
 	{
@@ -98,10 +35,10 @@ $('button').on('click', function(e)
 ]
 </textarea>
 
-		<button data-mirror="myCodeMirror" class="btn btn-success">Calculate total</button>
-	</div>
-	<div class="col-md-6">
+			<button data-mirror="myCodeMirror" class="btn btn-success">Calculate total</button>
+		</div>
 
+		<div class="col-md-6">
 <textarea id="textarea1" cols="20" rows="10" class="form-control default">
 [
 	{
@@ -117,8 +54,7 @@ $('button').on('click', function(e)
 ]
 </textarea>
 
-		<button data-mirror="myCodeMirror1" class="btn btn-success">Calculate total</button>
+			<button data-mirror="myCodeMirror1" class="btn btn-success">Calculate total</button>
+		</div>
 	</div>
-</div>
-
 @stop
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Conditions Demo</title>
+
+	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="csrf-token" content="{{ csrf_token() }}">
+
+	<title>Conditions Demo</title>
+
 	<link href="{{ URL::to('assets/css/bootstrap.min.css') }}" rel="stylesheet" media="screen">
 	<link href="{{ URL::to('assets/css/font-awesome.min.css') }}" rel="stylesheet" media="screen">
 	<link href="{{ URL::to('assets/css/demo.css') }}" rel="stylesheet" media="screen">
@@ -11,84 +15,128 @@
 
 	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
 	<!--[if lt IE 9]>
-	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-	<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<script src="//oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+		<script src="//oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
 	<![endif]-->
 
-	</head>
-	<body>
+</head>
+<body>
 
-		<div class="flux clearfix">
-			<div class="flux--1"></div>
-			<div class="flux--2"></div>
-			<div class="flux--3"></div>
-			<div class="flux--4"></div>
-			<div class="flux--5"></div>
-		</div>
+	<div class="flux clearfix">
+		<div class="flux--1"></div>
+		<div class="flux--2"></div>
+		<div class="flux--3"></div>
+		<div class="flux--4"></div>
+		<div class="flux--5"></div>
+	</div>
 
-		<div class="container">
-			<nav class="navbar xnavbar-fixed-top navbar-inverse" role="navigation">
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-					<a class="navbar-brand" href="{{ URL::to('/') }}">Conditions</a>
-				</div>
+	<div class="container">
+		<nav class="navbar xnavbar-fixed-top navbar-inverse" role="navigation">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
 
-				<div class="collapse navbar-collapse navbar-ex1-collapse">
-					<ul class="nav navbar-nav">
-						<li {{ URL::current() === URL::to('') ? 'class="active"' : null }}><a href="{{URL::to('/')}}">Home</a></li>
-						<li {{ URL::current() === URL::to('regular') ? 'class="active"' : null }}><a href="{{URL::to('regular')}}">Regular</a></li>
-						<li {{ URL::current() === URL::to('percentage') ? 'class="active"' : null }}><a href="{{URL::to('percentage')}}">Percentage</a></li>
-						<li {{ URL::current() === URL::to('inclusive') ? 'class="active"' : null }}><a href="{{URL::to('inclusive')}}">Inclusive</a></li>
-					</ul>
+				<a class="navbar-brand" href="{{ URL::to('/') }}">Conditions</a>
+			</div>
 
-					<ul class="nav navbar-nav navbar-right">
-						<li><a href="https://cartalyst.com/manual/conditions">Manual</a></li>
-					</ul>
-				</div>
-			</nav>
+			<div class="collapse navbar-collapse navbar-ex1-collapse">
+				<ul class="nav navbar-nav">
+					<li {!! URL::current() === URL::to('') ? 'class="active"' : null !!}><a href="{{URL::to('/')}}">Home</a></li>
+					<li {!! URL::current() === URL::to('regular') ? 'class="active"' : null !!}><a href="{{URL::to('regular')}}">Regular</a></li>
+					<li {!! URL::current() === URL::to('percentage') ? 'class="active"' : null !!}><a href="{{URL::to('percentage')}}">Percentage</a></li>
+					<li {!! URL::current() === URL::to('inclusive') ? 'class="active"' : null !!}><a href="{{URL::to('inclusive')}}">Inclusive</a></li>
+				</ul>
 
-			@yield('content')
-		</div>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="https://cartalyst.com/manual/conditions">Manual</a></li>
+				</ul>
+			</div>
+		</nav>
 
-		<script src="{{ URL::to('assets/js/jquery.min.js') }}"></script>
-		<script src="{{ URL::to('assets/js/bootstrap.min.js') }}"></script>
-		<script src="{{ URL::to('assets/js/codemirror.js') }}"></script>
-		<script src="{{ URL::to('assets/js/codemirror.javascript.js') }}"></script>
-		<script src="{{ URL::to('assets/js/waypoints.js') }}"></script>
-		<script src="{{ URL::to('assets/js/counterUp.js') }}"></script>
+		@yield('content')
+	</div>
 
-		<script type="text/javascript">
-			$('.tip').tooltip();
-		</script>
+	<script src="{{ URL::to('assets/js/jquery.min.js') }}"></script>
+	<script src="{{ URL::to('assets/js/bootstrap.min.js') }}"></script>
+	<script src="{{ URL::to('assets/js/codemirror.js') }}"></script>
+	<script src="{{ URL::to('assets/js/codemirror.javascript.js') }}"></script>
+	<script src="{{ URL::to('assets/js/waypoints.js') }}"></script>
+	<script src="{{ URL::to('assets/js/counterUp.js') }}"></script>
 
-		<script>
-		$.ajaxSetup({
-        headers: {
-            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        }
-	    });
-	    </script>
+	<script type="text/javascript">
+		var self = {};
 
-		@yield('scripts')
+		self.myCodeMirror = CodeMirror.fromTextArea(document.getElementById('textarea'), {
+			lineNumbers: true,
+			matchBrackets: true
+		});
 
-		<script type="text/javascript">
+		self.myCodeMirror1 = CodeMirror.fromTextArea(document.getElementById('textarea1'), {
+			lineNumbers: true,
+			matchBrackets: true
+		});
 
-			var _gaq = _gaq || [];
-			_gaq.push(['_setAccount', 'UA-26550564-1']);
-			_gaq.push(['_setDomainName', 'cartalyst.com']);
-			_gaq.push(['_trackPageview']);
+		$('button').on('click', function(e) {
+			e.preventDefault();
 
-			(function() {
-				var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-				ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-			})();
-		</script>
+			mirror = $(this).data('mirror');
 
-	</body>
+			var data = $.parseJSON(self[mirror].getValue());
+
+			console.log($.isArray(data));
+
+			if ($.isArray(data)) {
+				var oldData = data;
+				data = {};
+
+				$.each(oldData, function(i) {
+					data[i] = oldData[i];
+				});
+			}
+
+			$.ajaxSetup({
+				headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') }
+			});
+
+			$.ajax({
+				url: 'process',
+				data: data,
+				type: 'post',
+			}).done(function(res) {
+				$('h1 .tax').text(res.tax);
+				$('h1 .price').text(res.price);
+
+				$('.price').counterUp({
+					delay: 5,
+					time: 500
+				});
+
+				$('.tax').counterUp({
+					delay: 5,
+					time: 500
+				});
+			});
+		});
+	</script>
+
+	@yield('scripts')
+
+	<script type="text/javascript">
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-26550564-1']);
+		_gaq.push(['_setDomainName', 'cartalyst.com']);
+		_gaq.push(['_trackPageview']);
+
+		(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
+	</script>
+
+</body>
 </html>
